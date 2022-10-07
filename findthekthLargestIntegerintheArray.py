a7464ff4780e65952c1df335764fc202ec10aea6
Brook Habte <brookhabte5@gmail.com>
2022-10-07 03:06:36 +0300

    Delete findthekthLargestIntegerintheArray.py

--- findthekthLargestIntegerintheArray.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def kthLargestNumber(self, nums: List[str], k: int) -> str:
-        numsSorted = list(map(int,nums))
-        numsSorted.sort(reverse=True)
-        numsSortedStr = list(map(str,numsSorted))
-        return numsSortedStr[k-1]
-        
